a9a58d217e28695a8a02f5399167ee269ca95a1e
henryleo200333 <henryleo200333@gmail.com>
2024-11-01 16:09:57 +0800

    Product 修改功能

--- springboot-mall/src/main/java/com/kenleo/springboot_mall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/kenleo/springboot_mall/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,19 @@ public class ProductController {
 		Integer productId = productService.createProduct(productRequest);
 		Product product = productService.getProductById(productId);
 		return ResponseEntity.status(HttpStatus.CREATED).body(product);
-		
-		
 	}
+
+	@PutMapping("/products/{productId}")
+	public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+			                                     @RequestBody @Valid ProductRequest productRequest) {
+		//檢查是否存在
+		Product product = productService.getProductById(productId);
+		if(product == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+		
+		productService.updateProduct(productId, productRequest);
+		Product updatedProduct = productService.getProductById(productId);
+		return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+	}
+
 }
